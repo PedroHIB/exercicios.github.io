ab98a026fae04a06784612ebad1dee088c28ef58
pedroHIB <pedrohizib@gmail.com>
2020-01-10 17:30:52 -0300

    Exercicio 4 feito com sucesso

--- rocketseat/JS - Introducao JavaScript/Modulo 1/Exer4/javascript.js
+++ rocketseat/JS - Introducao JavaScript/Modulo 1/Exer4/javascript.js
@@ -1,10 +1,34 @@
-var txt = document.getElementById('box').value
+var txt = document.querySelector('#box')
 var btn = document.querySelector('#btn')
+var res = document.getElementById('resposta')
 
 btn.onclick = function (){
-	
-		alert(txt.value)
-	
+	if (txt.value >= 7){
+		res.innerHTML = 'Jedi Master'
+	} else {
+		switch(txt.value){
+			case '0':
+			res.innerHTML = 'Iniciante'
+			break;
+			case '1':
+			res.innerHTML = 'Iniciante => Intermediário'
+			break;
+			case '2':
+			res.innerHTML = 'Intermediário'
+			break;
+			case '3':
+			res.innerHTML = 'Intermediário => Avançado'
+			break;
+			case '4':
+			case '5':
+			case '6':
+			res.innerHTML = 'Avançado'
+			break;
+			default:
+			res.innerHTML = ''
+			alert('[ERROR] Coloque um valor validov no campo!')
+		}
+	}
 }
 
 /*Escreva uma função que dado um total de anos de estudo retorna o quão experiente o usuário é:
