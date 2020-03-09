66219d0f14cc5653153caf5cba419e46c9eb4376
pedroHIB <pedrohizib@gmail.com>
2020-03-09 15:47:04 -0300

    Conclusao do exercicio pratico 2 e 3

--- rocketseat/1.JS - Introducao JavaScript/Modulo 3/exer2 e exer 3/js.js
+++ rocketseat/1.JS - Introducao JavaScript/Modulo 3/exer2 e exer 3/js.js
@@ -1,31 +1,45 @@
-var listElement = document.querySelector('ul')
-var inputElement = document.querySelector('input')
+var elementUl = document.querySelector('ul')
 
+function renderLoading(loading) {
+    elementUl.innerHTML = ""
+
+    var textElement = document.createTextNode('Carregando...')
+    var loadingElement = document.createElement('li')
+
+    loadingElement.appendChild(textElement)
+    elementUl.appendChild(loadingElement)
+}
 
 function renderRepositories(repositories){
+    elementUl.innerHTML = "";
+    
     for(repo of repositories){
         const textElement = document.createTextNode(repo.name)
         const liElement = document.createElement('li')
 
         liElement.appendChild(textElement)
-        listElement.appendChild(liElement)
+        elementUl.appendChild(liElement)
     }
 }
 
 function ex(){
-    var user = inputElement.value
+    var user = document.querySelector('input').value
+
     if(!user) return
+    renderLoading()
+
+    console.log(user)
     axios.get(`https://api.github.com/users/${user}/repos`)
-	
-    .then(function (response) {
-    	renderRepositories(response.data)
+
+    .then(function(response) {
+        renderRepositories(response.data)
+    })
+    .catch(function(error) {
+        alert('[ERRO] 404')
     })
 }
 
-
-
-
-/*
+/*2º exercício
 Crie uma tela com um <input> que deve receber o nome de um usuário no Github. Após digitar o nome do usuário
 e clicar no botão buscar a aplicação deve buscar pela API do Github
 (conforme URL abaixo) os dados de repositórios do usuário e mostrá-los em tela:
@@ -46,4 +60,12 @@ Depois de preencher o input e adicionar, a seguinte lista deve aparecer abaixo:
  <li>repo4</li>
  <li>repo5</li>
 </ul>
-*/
+
+
+
+3º exercício
+A partir do resultado do exemplo anterior adicione um indicador de
+carregamento em tela no lugar da lista apenas enquanto a requisição
+estiver acontecendo:
+
+<li>Carregando...</li>*/
